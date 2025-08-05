b9c30c0808b4070cace609da28edbbc4ce3d692b
Divy Muni <divymuni@concret.io>
2025-08-05 16:32:08 +0530

    feat: promptify rollout initial commit

---
+++ test/commands/smockit/promptify.test.ts
@@ -0,0 +1,40 @@
+import { TestContext } from '@salesforce/core/testSetup';
+import { expect } from 'chai';
+import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
+import SmockitPromptify from '../../../src/commands/smockit/promptify.js';
+
+describe('smockit promptify', () => {
+  const $$ = new TestContext();
+  let sfCommandStubs: ReturnType<typeof stubSfCommandUx>;
+
+  beforeEach(() => {
+    sfCommandStubs = stubSfCommandUx($$.SANDBOX);
+  });
+
+  afterEach(() => {
+    $$.restore();
+  });
+
+  it('runs hello', async () => {
+    await SmockitPromptify.run([]);
+    const output = sfCommandStubs.log
+      .getCalls()
+      .flatMap((c) => c.args)
+      .join('\n');
+    expect(output).to.include('hello world');
+  });
+
+  it('runs hello with --json and no provided name', async () => {
+    const result = await SmockitPromptify.run([]);
+    expect(result.path).to.equal('src/commands/smockit/promptify.ts');
+  });
+
+  it('runs hello world --name Astro', async () => {
+    await SmockitPromptify.run(['--name', 'Astro']);
+    const output = sfCommandStubs.log
+      .getCalls()
+      .flatMap((c) => c.args)
+      .join('\n');
+    expect(output).to.include('hello Astro');
+  });
+});
